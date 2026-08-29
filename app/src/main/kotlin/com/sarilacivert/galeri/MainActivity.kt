20d6f08143db56d695a9b6850af7f74dc78c5edc
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-08-29 03:28:14 +0300

    perf: release thumbnail cache under memory pressure

--- app/src/main/kotlin/com/sarilacivert/galeri/MainActivity.kt
+++ app/src/main/kotlin/com/sarilacivert/galeri/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.sarilacivert.galeri
 
+import android.content.ComponentCallbacks2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
+import com.sarilacivert.galeri.data.BitmapLoader
 import com.sarilacivert.galeri.ui.GalleryApp
 import com.sarilacivert.galeri.ui.GalleryTheme
 
@@ -16,5 +18,17 @@ class MainActivity : ComponentActivity() {
                 GalleryApp()
             }
         }
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        if (level >= ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW) {
+            BitmapLoader.clearGlobalMemory()
+        }
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        BitmapLoader.clearGlobalMemory()
     }
 }
