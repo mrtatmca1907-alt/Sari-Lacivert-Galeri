a2e794bd95c88e3b8d492e017f27ea6dd27747da
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-08-29 04:54:32 +0300

    fix: restore normal file manager window bounds

--- app/src/main/kotlin/com/sarilacivert/galeri/MainActivity.kt
+++ app/src/main/kotlin/com/sarilacivert/galeri/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import com.sarilacivert.galeri.files.FastFileManagerApp
+import com.sarilacivert.galeri.files.WindowLayoutPolicy
 import com.sarilacivert.galeri.ui.GalleryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowCompat.setDecorFitsSystemWindows(window, !WindowLayoutPolicy.forceEdgeToEdge)
         setContent {
             GalleryTheme {
                 FastFileManagerApp()
