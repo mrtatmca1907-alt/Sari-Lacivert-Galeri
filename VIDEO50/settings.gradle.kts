3d0057e3bcd1438b7f0f983c27c6f71538014008
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-08-30 14:54:18 +0300

    build: scaffold Video50 project

---
+++ VIDEO50/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ATMACA-Video50"
+include(":app")
