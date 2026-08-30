a598b446d04139f4abf3383469cdaa40ae1d97fb
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-08-30 16:52:04 +0300

    release: ATMACA 1.2.0 direct Movies backup

--- ATMACA-WIFI/app/build.gradle.kts
+++ ATMACA-WIFI/app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.atmaca.files"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
