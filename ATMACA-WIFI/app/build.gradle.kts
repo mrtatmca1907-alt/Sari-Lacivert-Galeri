170703caf185e3a7508482672a391c5b2d94a208
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-08-31 02:16:37 +0300

    release: ATMACA 1.3.0 cloud to HDD recovery

--- ATMACA-WIFI/app/build.gradle.kts
+++ ATMACA-WIFI/app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.atmaca.files"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
