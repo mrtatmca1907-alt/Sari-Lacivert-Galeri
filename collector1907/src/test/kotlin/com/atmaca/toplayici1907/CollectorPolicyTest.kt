8b0afa0df8d2aec3c8df9e975669e9ed437e252a
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-08-31 03:48:04 +0300

    test: require collision-safe 1907 target names

--- collector1907/src/test/kotlin/com/atmaca/toplayici1907/CollectorPolicyTest.kt
+++ collector1907/src/test/kotlin/com/atmaca/toplayici1907/CollectorPolicyTest.kt
@@ -45,4 +45,17 @@ class CollectorPolicyTest {
     fun targetDetectionAcceptsMissingTrailingSlash() {
         assertTrue(CollectorPolicy.isTarget(p(1, "x.jpg", "Pictures/1907")))
     }
+
+    @Test
+    fun targetNamesPreserveExistingAndRenameIncomingConflict() {
+        val existing = p(10, "IMG.jpg", "Pictures/1907/")
+        val incoming = p(20, "IMG.jpg", "DCIM/Camera/")
+        val secondIncoming = p(30, "IMG.jpg", "Download/")
+
+        val names = CollectorPolicy.targetNames(listOf(incoming, existing, secondIncoming))
+
+        assertEquals("IMG.jpg", names[existing.id])
+        assertEquals("IMG_2.jpg", names[incoming.id])
+        assertEquals("IMG_3.jpg", names[secondIncoming.id])
+    }
 }
