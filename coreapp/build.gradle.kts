dc98915eb329ea112f5fb568a25f1e71c863b6d0
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-09-03 09:19:53 +0300

    chore: version full gallery build

--- coreapp/build.gradle.kts
+++ coreapp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.atmaca.gallery"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-core"
+        versionCode = 2
+        versionName = "0.2.0-full"
     }
 
     compileOptions {
